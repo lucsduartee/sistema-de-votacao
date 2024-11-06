77d3f71ad5b0658c3f707540b3e1f05a5b49330d
Lucas Duarte <lucs.rkt@gmail.com>
2024-11-06 13:42:50 -0300

    remove text

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -11,8 +11,3 @@ public abstract class Pessoa {
         this.nome = nome;
     }
 }
-// No projeto já existe um arquivo com a classe `com.betrybe.sistemadevotacao.Pessoa`. Nessa classe, você deve garantir que:
-//        1. Ela é uma classe abstrata, de forma que ela será utilizada como base para implementação de outras classes, mas não será instanciada por si.
-//        2. Ela possui o atributo protegido `nome` do tipo String.
-//        3. Ela possui os _getters_ e _setters_ correspondentes ao atributo.
-//  - Note que estes métodos não são abstratos, mesmo que a classe seja.
