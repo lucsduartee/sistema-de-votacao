3dcf1e36d88643c8f41b0a0d72010402044bdea3
Lucas Duarte <lucs.rkt@gmail.com>
2024-11-06 12:49:49 -0300

    feat(): implement 2 exercises

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,18 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+public abstract class Pessoa {
+    protected String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
+// No projeto já existe um arquivo com a classe `com.betrybe.sistemadevotacao.Pessoa`. Nessa classe, você deve garantir que:
+//        1. Ela é uma classe abstrata, de forma que ela será utilizada como base para implementação de outras classes, mas não será instanciada por si.
+//        2. Ela possui o atributo protegido `nome` do tipo String.
+//        3. Ela possui os _getters_ e _setters_ correspondentes ao atributo.
+//  - Note que estes métodos não são abstratos, mesmo que a classe seja.
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,28 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+    private int numero;
+    private int votos;
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void receberVoto() {
+        this.votos += 1;
+    }
+
+    public PessoaCandidata(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+        this.votos = 0;
+    }
 }
